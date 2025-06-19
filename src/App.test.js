ca759f556f6a204f891ec6b91c9f115833dcf998
saroj1973 <42770949+saroj1973@users.noreply.github.com>
2025-06-19 14:28:04 +0000

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = awaitscreen.findByText(/learn react/i);
-  
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
