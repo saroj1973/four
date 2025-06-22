fba934d332d4defda4c451d7ef1635b14443e8b3
saroj1973 <42770949+saroj1973@users.noreply.github.com>
2025-06-22 13:51:09 +0000

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 function App() {
   return (
+    <body bgcolor = "blue"> 
     <div className="App">
      <center> <h1>testing testing triggers saroj Shakya</h1> </center> 
     </div>
+  </body>
   );
 }
 export default App;
