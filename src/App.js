dbd4d871f38d650a9509b7ae45e16441350db9bc
saroj1973 <42770949+saroj1973@users.noreply.github.com>
2025-06-22 13:35:42 +0000

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 function App() {
   return (
     <div className="App">
-     <center> <h1>Hello from Saroj Shakya</h1> </center> 
-
+     <center> <h1>testing testing triggers  Shakya</h1> </center> 
     </div>
   );
 }
-
-  export default App;
+export default App;
