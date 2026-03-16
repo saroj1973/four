e931e6c13415341830822700792490fdccae820c
saroj1973 <42770949+saroj1973@users.noreply.github.com>
2026-03-16 11:28:38 +0000

    90007

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 function App() {
   return (
-    <body bgcolor = "red">
     <div className="App">
      <center> <h1> TESTING MCSIT SITE </h1> </center> 
     </div>
-    </body>
   );
 }
 export default App;
