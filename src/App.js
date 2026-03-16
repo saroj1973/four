2aa5a23f6f57293d35e4dbdd0df4ff5fa2fe1908
saroj1973 <42770949+saroj1973@users.noreply.github.com>
2026-03-16 06:22:03 +0000

    fw

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 function App() {
   return (
-    <body bgcolor = "blue"> 
     <div className="App">
      <center> <h1>testing testing triggers rm ram ram </h1> </center> 
     </div>
-  </body>
   );
 }
 export default App;
