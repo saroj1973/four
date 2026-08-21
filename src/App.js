17e8508d57f1d12ff9f83cf956af4a601fff5b81
saroj1973 <42770949+saroj1973@users.noreply.github.com>
2026-08-21 11:00:51 +0000

    dsffdsf

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 function App() {
   return (
-    <div className="App" style={{ backgroundColor: 'orange', minHeight: '100vh' }}>
-      {/* Your content goes here */}
-    </div>
+    <div className="App">
+         </div>
   );
 }
-
 export default App;
