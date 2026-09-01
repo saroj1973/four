eaa1f714cef5636e60e5b7338cf2ab7e6e73eab5
saroj1973 <42770949+saroj1973@users.noreply.github.com>
2026-09-01 07:36:38 +0000

    fdfd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
     <div className="App">
-         
-     </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+                      
+        </p>
+        <a
+          className="App-link"
+          href="https://www.ncit.edu.np"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          testing testing testing
+          Nepal College of Information Technology
+        </a>
+      </header>
+      
+    </div>
   );
 }
 export default App;
