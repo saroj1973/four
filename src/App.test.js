949d7cdfed5776ba329fa3ceaaa286112983c5ca
saroj1973 <42770949+saroj1973@users.noreply.github.com>
2026-09-01 09:09:59 +0000

    fdsdfds

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Testing NCIT Link link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Testing NCIT Link/i);
   expect(linkElement).toBeInTheDocument();
 });
